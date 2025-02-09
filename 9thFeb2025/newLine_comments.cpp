b3485f7b8ce4fceca44715c628be068ed52258f5
nikink334 <nikink334@gmail.com>
2025-02-09 22:55:28 +0530

    Initial commit

---
+++ 9thFeb2025/newLine_comments.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // using endl
+    cout << "Ron" << endl << "Weasely." <<endl;
+
+    // using backslash n (\n)
+    cout << "Harry\n" << "Potter.";
+
+    // in dsa we use \n while its consider that \n is faster than endl method.
+    // if we use shortcut (ctrl+/) then that line will be commented out.
+}
---
+++ 9thFeb2025/output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "This is the second lecture file.";
+
+    return 0;
+    
+}
